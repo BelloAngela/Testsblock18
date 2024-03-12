1eb8df84ccf990b559400cb526953d507cdbc7ca
BelloAngela <bellotile@mail.com>
2024-03-11 22:51:35 -0700

    Add files via upload

---
+++ tests.js
@@ -0,0 +1,43 @@
+//Unit Tests
+//A function called "multiplication" that returns the product of the two input numbers.
+//1.
+//Expect multiplication(2, 4) to be a number.
+//Expect multiplication(2, 4) to be equal to 8.
+//Expect multiplication(5, 0) to be equal to 0.
+//Expect multiplication(-4, 5) to be equal to -20.
+//Expect multiplication(0, 0) to be equal to 0.
+//Expect multiplication("a", 2) to be an error.
+
+//A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+//expect ([1,2,3,4,5] [4,2,6,7,1,3,9,0]) to return ([1,3,5,7,9])
+//expect string input to return error
+//expect multiples of the same number to return the number once
+//expect negative numbers to be returned as well
+//expect more than two arrays to return error
+//expect one array to be returned on its own
+//expect no input to return error
+
+
+
+
+// functional Test 
+
+// When user clicks on shopping cart a list of the added items should appear 
+// if the cart is empty an option to return to shopping should appear and reccomended items shown
+// user is shown free shipping over $50 purchase
+// when user clicks on checkout a prompt should appear that requires "login to checkout", or "checkout as guest"
+// when user selects "log in to checkout", a new page should appear to enter (user name and password)
+// when user scrolls down on the shopping cart there should be a button for checkout 
+// when user selects "checkout as a guest" a new page appears with all the items of the shopping cart and a total for the amount to pay
+// when in the check out as a guest page user should have an option to create and account with details of why it is more convenient to have an account for future purchases 
+// when  in the checkout as a guest page user should have an option to log in 
+// when user has reviewed the items and is ready to pay they can click on checkout tab
+// when clicked checkout user will be directed to shipping address input and delivery options
+// when user clicks on check out they should be taken to a new window for authentication of payment
+// when user is in new window there will be options for payments (credit card, debit card)
+// when user is in the payment window user will need to fill out payment info and billing info (name, address, email, phone number, if gift option)
+// when the user takes more than 10 minutes in the payment window, the user will be taken back to the cart window to restart process items will be auto saved if signed in 
+// when authentication process fails, allow the user to re-enter their payment information
+// when payment fails 5 times in a row, take user back to cart window
+// when payment is approved, the user will receive an email confirmation of order number and details of the order and eta of delivery
+ 
